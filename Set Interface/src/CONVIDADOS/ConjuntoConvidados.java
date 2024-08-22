4a06d0027181dae1a8517d20244db5c4c22dbbef
Shellbaka <shellbaka69@gmail.com>
2024-08-22 09:15:18 -0300

    Primeiro commit, preparando uma interface Set.

---
+++ Set Interface/src/CONVIDADOS/ConjuntoConvidados.java
@@ -0,0 +1,44 @@
+package CONVIDADOS;
+import java.util.Set;
+import java.util.HashSet;
+
+public class ConjuntoConvidados {
+    
+private Set<Convidado> convidadoSet;
+
+
+public ConjuntoConvidados () {
+    this.convidadoSet = new HashSet<>();
+}
+
+
+public void adcionarUmConvidadoNoSet(String name, int codigoConvidado) {
+convidadoSet.add(new Convidado(name, codigoConvidado));
+}
+
+
+public void removerCodigoConvite(int codigoConvidado) {
+Convidado convidadoParaRemover = null;
+for(Convidado c : convidadoSet) {
+    if(c.getCodigoConvidado() == codigoConvidado) {
+        convidadoParaRemover = c;
+        break;
+    }
+}
+convidadoSet.remove(convidadoParaRemover);
+
+
+}
+
+
+public int contarConvidados(){
+    return convidadoSet.size();
+}
+
+public void exibirConvidados() {
+    System.out.println(convidadoSet);
+}
+
+
+
+}
---
+++ Set Interface/src/CONVIDADOS/Convidado.java
@@ -0,0 +1,34 @@
+package CONVIDADOS;
+
+public class Convidado {
+    
+    private String name;
+
+    private int codigoConvidado;
+
+    public Convidado(String name, int codigoConvidado){
+      this.name = name;
+      this.codigoConvidado = codigoConvidado;
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+public int getCodigoConvidado() {
+    return codigoConvidado;
+
+}
+
+@Override
+public String toString() {
+    
+    return "Nome= " + name + "\n" +
+            "Código de convidado= " + codigoConvidado + "\n" ;
+}
+
+
+
+}
